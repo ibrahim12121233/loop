aeed43c7694eea2f1d921225ae10e2e48b8db733
ibrahim <nsism99@gmail.com>
2023-08-17 16:07:25 +0300

    done

--- mein.js
+++ mein.js
@@ -2,24 +2,24 @@
 //     if (a == 10) {
 //         break;
 //     }
-//     console.log(a);
+        // alert(a)
 // }
 
 // for (let i = 1; i <= 20; i++) {
 //     if (i == 10) {
 //         continue;
 //     }
-    //  console.log(i);
+// alert(i)
 // }
 
 // let z =1;
 //  while(z<=3){
-//      console.log(z);
+// alert(z)
 //     z++;
 //  }
 
 // let v =1;
 //  do{
-//      console.log(v);
+    //  alert(v)
 //      v++;
 //  }while(v<=5);
