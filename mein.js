33ed304d003627ca1be52b7cbba6db41bfafc3dc
ibrahim <nsism99@gmail.com>
2023-08-17 14:11:13 +0300

    done

---
+++ mein.js
@@ -0,0 +1,25 @@
+// for (let a = 1; a <= 30; a++) {
+//     if (a == 10) {
+//         break;
+//     }
+//     console.log(a);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 10) {
+//         continue;
+//     }
+    //  console.log(i);
+// }
+
+// let z =1;
+//  while(z<=3){
+//      console.log(z);
+//     z++;
+//  }
+
+// let v =1;
+//  do{
+//      console.log(v);
+//      v++;
+//  }while(v<=5);
